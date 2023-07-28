116b1ddea2458028f6d3d4a393ea0665bbe89a41
Todd Cooke <toddcookevt@gmail.com>
2023-07-28 13:56:55 -0400

    check pip for package

--- app/page.js
+++ app/page.js
@@ -1,31 +1,48 @@
 "use client";
 
-import Image from "next/image";
 import styles from "./page.module.css";
 import { useState } from "react";
 
 export default function Home() {
   const [text, setText] = useState("");
   const [existsInGithub, setExistsInGithub] = useState(null);
+  const [existsInPip, setExistsInPip] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  async function existsInPip(packageName) {
-    const res = await fetch(`https://pypi.org/pypi/${packageName}/json`);
-    return res.status === 200;
+  async function searchPip(packageName) {
+    console.log("xxx", packageName);
+
+    let resp;
+    try {
+      resp = await fetch(`https://pypi.org/pypi/${packageName}/json`, {
+        headers: {
+          Host: "pypi.org",
+          Accept: "application/json",
+        },
+      });
+    } catch (e) {
+      console.log(e);
+      return;
+    }
+
+    // const json = await resp.json();
+    console.log("xxx", resp);
+    if (resp.status === 200) setExistsInPip(true);
+    else setExistsInPip(false);
   }
 
   async function searchGithub(repoName) {
     const res = await fetch(`/api/exists/github?repo=${repoName}`);
     const json = await res.json();
-
-    console.log("xxx", json);
-
     setExistsInGithub(json.exists);
   }
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
+    setLoading(true);
     console.log("handling submit...");
-    searchGithub(text);
+    await Promise.all([searchGithub(text), searchPip(text)]);
+    setLoading(false);
   }
 
   return (
@@ -42,9 +59,20 @@ export default function Home() {
         </button>
       </form>
 
-      {existsInGithub && <p>❌ GitHub repo already exists</p>}
-      {existsInGithub !== null && !existsInGithub && (
-        <p>✅GitHub repo name is available!</p>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <>
+          {existsInGithub && <p>❌ GitHub repo already exists</p>}
+          {existsInGithub !== null && !existsInGithub && (
+            <p>✅GitHub repo name is available!</p>
+          )}
+
+          {existsInPip && <p>❌ Pip package already exists</p>}
+          {existsInPip !== null && !existsInPip && (
+            <p>✅Pip package name is available!</p>
+          )}
+        </>
       )}
     </main>
   );
