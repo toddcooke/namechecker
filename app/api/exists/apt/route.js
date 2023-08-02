ac288caf573591719fb407059d80a0b7a21f3e8f
Todd Cooke <toddcookevt@gmail.com>
2023-08-02 13:29:31 -0400

    try path.resolve

--- app/api/exists/apt/route.js
+++ app/api/exists/apt/route.js
@@ -6,8 +6,10 @@ import * as path from "path";
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const name = searchParams.get("name");
+  const dir = path.resolve("./public", ".");
+  console.log(dir);
   let aptPackagesPath = process.env.VERCEL
-    ? "/apt-packages.txt"
+    ? path.join(dir, "apt-packages.txt")
     : "public/apt-packages.txt";
   const lines = await promises.readFile(aptPackagesPath, "utf-8");
   const found = lines.includes(name);
