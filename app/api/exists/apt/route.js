b3a2225bcd1dd704c05bd285fd279b11aa4f8d8d
Todd Cooke <toddcookevt@gmail.com>
2023-08-02 13:36:56 -0400

    clean up apt handler

--- app/api/exists/apt/route.js
+++ app/api/exists/apt/route.js
@@ -3,15 +3,15 @@ import { NextResponse } from "next/server";
 import { promises } from "fs";
 import * as path from "path";
 
+const dir = path.resolve("./public", ".");
+const aptPackagesPath = process.env.VERCEL
+  ? path.join(dir, "apt-packages.txt")
+  : "public/apt-packages.txt";
+const packages = await promises.readFile(aptPackagesPath, "utf-8");
+
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const name = searchParams.get("name");
-  const dir = path.resolve("./public", ".");
-  console.log(dir);
-  let aptPackagesPath = process.env.VERCEL
-    ? path.join(dir, "apt-packages.txt")
-    : "public/apt-packages.txt";
-  const lines = await promises.readFile(aptPackagesPath, "utf-8");
-  const found = lines.includes(name);
+  const found = packages.includes(name);
   return NextResponse.json({ exists: found });
 }
