dc296800c36a7a60d65c64770a2fc7133dd3fc28
Todd Cooke <toddcookevt@gmail.com>
2023-08-02 12:12:53 -0400

    use relative path to apt-packages.txt attempt #2

--- app/api/exists/apt/route.js
+++ app/api/exists/apt/route.js
@@ -1,13 +1,15 @@
 import "server-only";
 import { NextResponse } from "next/server";
 import { promises } from "fs";
+import { serverRuntimeConfig } from "@/next.config";
+import * as path from "path";
 
 // This needs to be a server-side route because fs in next.js is only available server-side
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const name = searchParams.get("name");
   let aptPackagesPath = process.env.VERCEL
-    ? "../../../../public/apt-packages.txt"
+    ? path.join(serverRuntimeConfig.PROJECT_ROOT, "../../../apt-packages.txt")
     : "public/apt-packages.txt";
   const lines = await promises.readFile(aptPackagesPath, "utf-8");
   const found = lines.includes(name);
