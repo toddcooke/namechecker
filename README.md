ef32d96013030d0504c8dc3bb882a915617d5469
Todd Cooke <toddcookevt@gmail.com>
2023-08-04 09:40:09 -0400

    update readme add link to github repo

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Name Checker
-This is a tool for finding out if a project name is taken already.
+This is a tool for finding out if a project name is taken already. Instead of checking apt, npm, GitHub, etc, for the name of your project, you can use this tool to search many places at once.
--- app/TailwindLayout.js
+++ app/TailwindLayout.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function TailwindLayout({ children }) {
   return (
@@ -8,9 +9,11 @@ export default function TailwindLayout({ children }) {
         <div className="py-10">
           <header>
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-              <h1 className="text-3xl font-bold leading-tight tracking-tight text-center text-gray-900 dark:text-amber-100">
-                Name Checker
-              </h1>
+              <Link href={"https://github.com/toddcooke/namechecker"}>
+                <h1 className="text-3xl font-bold leading-tight tracking-tight text-center text-gray-900 dark:text-amber-100">
+                  Name Checker
+                </h1>
+              </Link>
               <h2 className={"text-center p-1 dark:text-amber-100"}>
                 Find out if your project name is taken
               </h2>
