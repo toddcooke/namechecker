e810998f07018990b62aaa0b7c83dc8f685279d5
Todd Cooke <toddcookevt@gmail.com>
2023-08-22 16:55:55 -0400

    handle GitLab sometimes throttling requests

--- app/api/exists/gitlab/route.js
+++ app/api/exists/gitlab/route.js
@@ -5,22 +5,28 @@ import { NextResponse } from 'next/server';
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const name = searchParams.get('name');
-  for (let page = 1; page <= 10; page++) {
-    const response = await fetch(
-      `https://gitlab.com/api/v4/projects?search="${name}"&per_page=100&page=${page}`,
-    );
-    const json = await response.json();
-    if (json.length === 0) break;
-    const url = json.find((value) => value.name === name)?.web_url;
-    if (url) {
-      return NextResponse.json({
-        exists: true,
-        existsUrl: url,
-      });
+  try {
+    for (let page = 1; page <= 10; page++) {
+      const response = await fetch(
+        `https://gitlab.com/api/v4/projects?search="${name}"&per_page=100&page=${page}`,
+      );
+      const json = await response.json();
+      if (json.length === 0) break;
+      const url = json.find((value) => value.name === name)?.web_url;
+      if (url) {
+        return NextResponse.json({
+          exists: true,
+          existsUrl: url,
+        });
+      }
     }
+    return NextResponse.json({
+      exists: false,
+    });
+  } catch (e) {
+    // GitLab doesn't like their api getting spammed, but they don't have a projects/<name> endpoint.
+    // So if we get an error we'll just show nothing.
+    console.error(e);
+    return NextResponse.json(null);
   }
-
-  return NextResponse.json({
-    exists: false,
-  });
 }
