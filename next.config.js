f8016b2e4180edb48a780b1f8d909e6abe3897be
Todd Cooke <toddcookevt@gmail.com>
2024-07-22 07:02:25 -0400

    add node types

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   serverRuntimeConfig: {
     PROJECT_ROOT: __dirname,
   },
+  experimental: {
+    // Temporary fix for https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 module.exports = nextConfig;
