022ce83ab7c3f48a4355363c3c45f2e36760c4b8
Todd Cooke <toddcookevt@gmail.com>
2025-05-25 19:13:38 -0400

    bump react

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
     PROJECT_ROOT: __dirname,
   },
   experimental: {
-    // Temporary fix for https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
-    missingSuspenseWithCSRBailout: false,
   },
 };
 
