45332ea9ad1f4d33b65ca19608840b891e10b3cb
Todd Cooke <toddcookevt@gmail.com>
2025-05-25 19:21:53 -0400

    fix tests

--- tests/domain.spec.js
+++ tests/domain.spec.js
@@ -5,17 +5,8 @@ const { test, expect } = require('@playwright/test');
 test('domain taken', async ({ page }) => {
   await page.goto('http://localhost:3000');
   const search = await page.locator('#name-search');
-  await search.type('sk.org');
+  await search.fill('asdf');
   await page.locator('#search-button').click();
-  await expect(page.getByText(`sk.org already exists`)).toBeVisible();
-});
-
-test('domain available', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-  const search = await page.locator('#name-search');
-  await search.type('some-long-unused-domain.com');
-  await page.locator('#search-button').click();
-  await expect(
-    page.getByText(`some-long-unused-domain.com is available`),
-  ).toBeVisible();
+  await expect(page.getByText(`Apt name is available`)).toBeVisible();
+  await expect(page.getByText(`PyPI name already exists`)).toBeVisible();
 });
